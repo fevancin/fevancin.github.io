8d593d08e5695a07bf7b157168650b3463939dc4
Federico Vancini <56303504+fevancin@users.noreply.github.com>
2021-04-16 15:56:59 +0200

    corrected main.js

--- main.js
+++ main.js
@@ -3,22 +3,74 @@
 const MAZE_DIM = 9;
 
 const canvas = document.getElementById("drawing-canvas");
-if (typeof canvas === "undefined" || canvas === null) {
+if (typeof canvas === "undefined") {
   console.error("Canvas element not found");
 }
 
 const button = document.getElementById("draw-button");
-if (typeof button === "undefined" || button === null) {
+if (typeof button === "undefined") {
   console.error("Button element not found");
 }
 
 // resize the canvas to the right pixel resolution
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
-/* canvas.addEventListener("resize", () => {
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
-}); */
+
+function drawMaze(maze) {
+  const ctx = canvas.getContext("2d");
+  const px = canvas.width / (MAZE_DIM * 2 + 1); // square room size
+  ctx.fillStyle = "#4d4d4d";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = "#000000";
+  ctx.fillStyle = "#757575";
+  ctx.lineWidth = 4;
+
+  // draws the outlines of the path
+  for (let i = 0; i < MAZE_DIM; i++) {
+    for (let j = 0; j < MAZE_DIM; j++) {
+      if (maze[i][j].visited) {
+        ctx.strokeRect(px * (i * 2 + 1), px * (j * 2 + 1), px, px);
+         if (maze[i][j].parent !== null) {
+          if (i > 0 && maze[i][j].parent.x === i - 1 && maze[i][j].parent.y === j) {
+            ctx.strokeRect(px * (i * 2), px * (j * 2 + 1), px, px);
+          }
+          if (i < MAZE_DIM - 1 && maze[i][j].parent.x === i + 1 && maze[i][j].parent.y === j) {
+            ctx.strokeRect(px * (i * 2 + 2), px * (j * 2 + 1), px, px);
+          }
+          if (j > 0 && maze[i][j].parent.x === i && maze[i][j].parent.y === j - 1) {
+            ctx.strokeRect(px * (i * 2 + 1), px * (j * 2), px, px);
+          }
+          if (j < MAZE_DIM - 1 && maze[i][j].parent.x === i && maze[i][j].parent.y === j + 1) {
+            ctx.strokeRect(px * (i * 2 + 1), px * (j * 2 + 2), px, px);
+          }
+        }
+      }
+    }
+  }
+
+  // draws the inside path of the maze
+  for (let i = 0; i < MAZE_DIM; i++) {
+    for (let j = 0; j < MAZE_DIM; j++) {
+      if (maze[i][j].visited) {
+        ctx.fillRect(px * (i * 2 + 1) + 2, px * (j * 2 + 1) + 2, px - 4, px - 4);
+        if (maze[i][j].parent !== null) {
+          if (i > 0 && maze[i][j].parent.x === i - 1 && maze[i][j].parent.y === j) {
+            ctx.fillRect(px * (i * 2) - 3, px * (j * 2 + 1) + 2, px + 6, px - 4);
+          }
+          if (i < MAZE_DIM - 1 && maze[i][j].parent.x === i + 1 && maze[i][j].parent.y === j) {
+            ctx.fillRect(px * (i * 2 + 2) - 3, px * (j * 2 + 1) + 2, px + 6, px - 4);
+          }
+          if (j > 0 && maze[i][j].parent.x === i && maze[i][j].parent.y === j - 1) {
+            ctx.fillRect(px * (i * 2 + 1) + 2, px * (j * 2) - 3, px - 4, px + 6);
+          }
+          if (j < MAZE_DIM - 1 && maze[i][j].parent.x === i && maze[i][j].parent.y === j + 1) {
+            ctx.fillRect(px * (i * 2 + 1) + 2, px * (j * 2 + 2) - 3, px - 4, px + 6);
+          }
+        }
+      }
+    }
+  }
+}
 
 // when the button is clicked...
 button.addEventListener("click", () => {
@@ -29,142 +81,68 @@ button.addEventListener("click", () => {
     const row = [];
     for (let j = 0; j < MAZE_DIM; j++) {
       row.push({
-        visited: false,       // true for finished rooms
-        parent: null,      // the room we came from in the visit
-        neighboursToDo: [] // neighbours with 'visited = false'
+        visited: false,    // true for visited rooms
+        parent: null, // the room coordinate we came from in the visit
+        neighboursToDo: 4  // neighbours with 'visited = false'
       });
+      if (i === 0 || i === MAZE_DIM - 1) row[j].neighboursToDo--;
+      if (j === 0 || j === MAZE_DIM - 1) row[j].neighboursToDo--;
     }
     maze.push(row);
   }
 
-  // setting 'neighboursToDo' values
-  for (let i = 0; i < MAZE_DIM; i++) {
-    for (let j = 0; j < MAZE_DIM; j++) {
-      if (i > 0) {
-        maze[i][j].neighboursToDo.push(maze[i - 1][j]);
-      }
-      if (i < MAZE_DIM - 1) {
-        maze[i][j].neighboursToDo.push(maze[i + 1][j]);
-      }
-      if (j > 0) {
-        maze[i][j].neighboursToDo.push(maze[i][j - 1]);
-      }
-      if (j < MAZE_DIM - 1) {
-        maze[i][j].neighboursToDo.push(maze[i][j + 1]);
-      }
-    }
-  }
-
   // starting a depth-first search from room (0, 0)
-  let currentRoom = maze[0][0];
-  let n = 0;
-  while (currentRoom !== null) {
-    
-    
-    if (n > 1000) break;
-    n++;
-    
-    // mark as visited the current room and remove it from all neighbours
-    if (!currentRoom.visited) {
-      currentRoom.visited = true;
-      currentRoom.neighboursToDo.forEach((neighbour) => {
-        neighbour.neighboursToDo = neighbour.neighboursToDo.filter((room) => {
-          return room !== currentRoom;
-        });
-      });
+  let c = {x: 0, y: 0};
+  do {
+
+    // mark the room as visited and remove 1 from all neighbour numbers
+    let room = maze[c.x][c.y];
+    if (!room.visited) {
+      room.visited = true;
+      if (c.x > 0) {
+        maze[c.x - 1][c.y].neighboursToDo--;
+      }
+      if (c.x < MAZE_DIM - 1) {
+        maze[c.x + 1][c.y].neighboursToDo--;
+      }
+      if (c.y > 0) {
+        maze[c.x][c.y - 1].neighboursToDo--;
+      }
+      if (c.y < MAZE_DIM - 1) {
+        maze[c.x][c.y + 1].neighboursToDo--;
+      }
     }
 
-    if (currentRoom.neighboursToDo.length === 0) { // retreat case
-      currentRoom = currentRoom.parent;
+    if (room.neighboursToDo === 0) { // retreat to parent
+      c = {x: room.parent.x, y: room.parent.y};
       continue;
     }
-    
-    // select a neighbour from the list as the next room
-    const randomIndex = Math.floor(Math.random() * currentRoom.neighboursToDo.length);
-    const nextRoom = currentRoom.neighboursToDo[randomIndex];
-    nextRoom.parent = currentRoom;
-    currentRoom = nextRoom;
 
-  }
+    // select a neighbour from the list as the next room
+    let r = Math.floor(Math.random() * room.neighboursToDo);
+    const nextC = {x: c.x, y: c.y}; // new coordinate
+    if (c.x > 0 && maze[c.x - 1][c.y].visited === false) {
+      r--;
+      if (r === -1) nextC.x--;
+    }
+    if (c.x < MAZE_DIM - 1 && maze[c.x + 1][c.y].visited === false) {
+      r--;
+      if (r === -1) nextC.x++;
+    }
+    if (c.y > 0 && maze[c.x][c.y - 1].visited === false) {
+      r--;
+      if (r === -1) nextC.y--;
+    }
+    if (c.y < MAZE_DIM - 1 && maze[c.x][c.y + 1].visited === false) {
+      r--;
+      if (r === -1) nextC.y++;
+    }
+    maze[nextC.x][nextC.y].parent = {x: c.x, y: c.y}; // set the return path
+    c = {x: nextC.x, y: nextC.y}; // move to next coordinate
+
+  } while (c.x !== 0 || c.y !== 0); // repeat until back to start
 
   // draw the maze into the canvas
-  const ctx = canvas.getContext("2d");
-  const px = canvas.width / (MAZE_DIM * 2 + 1);
-  ctx.fillStyle = "#4d4d4d";
-  ctx.strokeStyle = "#000000";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.lineWidth = 4;
-  ctx.fillStyle = "#757575";
-  // draws the outlines of the path
-  for (let i = 0; i < MAZE_DIM; i++) {
-    for (let j = 0; j < MAZE_DIM; j++) {
-      if (maze[i][j].visited) {
-        ctx.strokeRect(px * (i * 2 + 1), px * (j * 2 + 1), px, px);
-         if (maze[i][j].parent !== null) {
-          if (i > 0 && maze[i][j].parent === maze[i - 1][j]) {
-            ctx.beginPath();
-            ctx.moveTo(px * (i * 2) - 4, px * (j * 2 + 1));
-            ctx.lineTo(px * (i * 2 + 1) + 4, px * (j * 2 + 1));
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(px * (i * 2) - 4, px * (j * 2 + 2));
-            ctx.lineTo(px * (i * 2 + 1) + 4, px * (j * 2 + 2));
-            ctx.stroke();
-          }
-          if (i < MAZE_DIM - 1 && maze[i][j].parent === maze[i + 1][j]) {
-            ctx.beginPath();
-            ctx.moveTo(px * (i * 2 + 2) - 4, px * (j * 2 + 1));
-            ctx.lineTo(px * (i * 2 + 3) + 4, px * (j * 2 + 1));
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(px * (i * 2 + 2) - 4, px * (j * 2 + 2));
-            ctx.lineTo(px * (i * 2 + 3) + 4, px * (j * 2 + 2));
-            ctx.stroke();
-          }
-          if (j > 0 && maze[i][j].parent === maze[i][j - 1]) {
-            ctx.beginPath();
-            ctx.moveTo(px * (i * 2 + 1), px * (j * 2) - 4);
-            ctx.lineTo(px * (i * 2 + 1), px * (j * 2 + 1) + 4);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(px * (i * 2 + 2), px * (j * 2) - 4);
-            ctx.lineTo(px * (i * 2 + 2), px * (j * 2 + 1) + 4);
-            ctx.stroke();
-          }
-          if (j < MAZE_DIM - 1 && maze[i][j].parent === maze[i][j + 1]) {
-            ctx.beginPath();
-            ctx.moveTo(px * (i * 2 + 1), px * (j * 2 + 2) - 4);
-            ctx.lineTo(px * (i * 2 + 1), px * (j * 2 + 3) + 4);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(px * (i * 2 + 2), px * (j * 2 + 2) - 4);
-            ctx.lineTo(px * (i * 2 + 2), px * (j * 2 + 3) + 4);
-            ctx.stroke();
-          }
-        }
-      }
-    }
-  }
-  // draws the inside of the maze
-  for (let i = 0; i < MAZE_DIM; i++) {
-    for (let j = 0; j < MAZE_DIM; j++) {
-      if (maze[i][j].visited) {
-        ctx.fillRect(px * (i * 2 + 1) + 2, px * (j * 2 + 1) + 2, px - 4, px - 4);
-        if (maze[i][j].parent !== null) {
-          if (i > 0 && maze[i][j].parent === maze[i - 1][j]) {
-            ctx.fillRect(px * (i * 2) - 3, px * (j * 2 + 1) + 2, px + 6, px - 4);
-          }
-          if (i < MAZE_DIM - 1 && maze[i][j].parent === maze[i + 1][j]) {
-            ctx.fillRect(px * (i * 2 + 2) - 3, px * (j * 2 + 1) + 2, px + 6, px - 4);
-          }
-          if (j > 0 && maze[i][j].parent === maze[i][j - 1]) {
-            ctx.fillRect(px * (i * 2 + 1) + 2, px * (j * 2) - 3, px - 4, px + 6);
-          }
-          if (j < MAZE_DIM - 1 && maze[i][j].parent === maze[i][j + 1]) {
-            ctx.fillRect(px * (i * 2 + 1) + 2, px * (j * 2 + 2) - 3, px - 4, px + 6);
-          }
-        }
-      }
-    }
-  }
+  drawMaze(maze);
+
 });
