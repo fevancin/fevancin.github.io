7555e53846955b0fb957710bffec906d34290224
Federico Vancini <56303504+fevancin@users.noreply.github.com>
2021-07-30 00:36:35 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,149 +1,124 @@
 "use strict";
 
-const cards = [
-    "G",
-    "G",
-    "G",
-    "G",
-    "C",
-    "C",
-    "C",
-    "C",
-    "M",
-    "M",
-    "G P",
-    "G P",
-    "G P",
-    "C P",
-    "C P",
-    "C P",
-    "G M",
-    "G M",
-    "G M",
-    "G M",
-    "G M",
-    "G C+",
-    "G C+",
-    "G C",
-    "G C",
-    "P C+",
-    "C A",
-    "C A",
-    "C A",
-    "C A",
-    "G A",
-    "G A",
-    "G A",
-    "G A",
-    "C M",
-    "C M",
-    "C M",
-    "C M",
-    "C M",
-    "C G+",
-    "C G+",
-    "P M",
-    "P G+",
-    "G C M+2",
-    "G C+ M+2",
-    "G C+ M+2",
-    "C G+ M+2",
-    "C G+ M+2",
-    "C G P+",
-    "G P+ M",
-    "C G P",
-    "G C A",
-    "2 C A",
-    "2 G A",
-    "2 G C+ P+",
-    "2 G+ P+",
-    "2 G+ C+ A",
-    "2 C A P+",
-    "2 G A P+",
-    "2 C C+ P+",
-    "2 G G+ P+",
-    "2 C+ P+ M",
-    "2 G+ P+ M",
-    "2 C C+ A",
-    "2 G G+ A",
-    "2 C A M",
-    "2 G A M",
-    "2 P A M",
-    "2 G G+ A M+3",
-    "2 C C+ A M+3",
-    "2 G C+ A M+3",
-    "2 C G+ A M+3",
-    "2 G M P+ M+2",
-    "2 C P P+ M+2"
-];
-
-const descriptions = {
-    "C": "Caserma",
-    "G": "Campo di grano",
-    "M": "Municipio",
-    "P": "Porto",
-    "C+": "Migliora Caserma",
-    "G+": "Migliora Campo di grano",
-    "M+2": "Migliora Municipio al livello 2",
-    "M+3": "Migliora Municipio al livello 3",
-    "P+": "Migliora Porto",
-    "A": "Attacco"
+const constants = {
+    archers: 100,
+    knights: 50,
+    catapults: 40,
+    dragons: 30,
+    castles: 8,
+    halls: 27
 };
-
-const circleClasses = {
-    "C": "barracks",
-    "G": "wheat",
-    "M": "hall",
-    "P": "harbor",
-    "C+": "barracks",
-    "G+": "wheat",
-    "M+2": "hall",
-    "M+3": "hall",
-    "P+": "harbor",
-    "A": "attack"
-};
-
-const betweenWord = " o <br>";
 
 const body = document.querySelector("body");
-let i = 0;
-let cardContainer = null;
 
-cards.forEach((card) => {
-    if (i % 9 === 0) {
-        cardContainer = document.createElement("div");
-        body.appendChild(cardContainer);
-        cardContainer.classList.add("card-container");
-    }
-    const cardElement = document.createElement("div");
-    let circles = card.split(" ");
-    if (circles[0] === "2") {
-        circles.shift();
-        cardElement.classList.add("second");
-    }
-    switch (circles.length) {
-        case 1: cardElement.classList.add("single"); break;
-        case 2: cardElement.classList.add("double"); break;
-        case 3: cardElement.classList.add("triple"); break;
-        case 4: cardElement.classList.add("quadruple"); break;
-    }
-    cardElement.classList.add("card");
-    let string = "";
-    circles.forEach((circle, index) => {
-        const circleElement = document.createElement("div");
-        circleElement.classList.add(circleClasses[circle]);
-        circleElement.classList.add("circle");
-        if (circle.length > 1) circleElement.classList.add("plus");
-        cardElement.appendChild(circleElement);
-        string += descriptions[circle];
-        if (index < circles.length - 1) {
-            string += betweenWord;
-        }
-    });
-    const descriptionElement = document.createElement("div");
-    descriptionElement.innerHTML = string;
-    descriptionElement.classList.add("text");
-    cardElement.appendChild(descriptionElement);
-    cardContainer.appendChild(cardElement);
-    i++;
-});
+let containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.archers; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("archer");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "🏹";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
+containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.archers; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("archer");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "1";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
+containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.knights; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("knight");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "🐎";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
+containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.knights; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("knight");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "3";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
+containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.catapults; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("catapult");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "💣";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
+containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.catapults; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("catapult");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "5";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
+containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.dragons; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("dragon");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "🐉";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
+containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.dragons; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("dragon");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "8";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
+containerElement = document.createElement("div");
+containerElement.classList.add("unit-container");
+for (let i = 0; i < constants.castles; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("castle");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "🏰";
+    containerElement.appendChild(unitElement);
+}
+for (let i = 0; i < constants.halls; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("hall");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "🏛️";
+    containerElement.appendChild(unitElement);
+}
+for (let i = 0; i < constants.castles; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("castle");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "🗺️";
+    containerElement.appendChild(unitElement);
+}
+for (let i = 0; i < constants.halls; i++) {
+    const unitElement = document.createElement("div");
+    unitElement.classList.add("hall");
+    unitElement.classList.add("unit");
+    unitElement.innerHTML = "🗺️";
+    containerElement.appendChild(unitElement);
+}
+body.appendChild(containerElement);
