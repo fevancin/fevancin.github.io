379dee1432edda8a3ccd80122ff2d05936793789
Federico Vancini <56303504+fevancin@users.noreply.github.com>
2021-09-18 22:50:54 +0200

    Add files via upload

--- js/numbers.js
+++ js/numbers.js
@@ -17,10 +17,11 @@
 const FIRST_DECK_DIM = 30;
 
 const cards = [
-    "W", "W", "W", "W", "B", "B", "B", "B", "W B", "W B", "W B", "W B",
+    "W", "W", "W", "B", "B", "B", "W B", "W B", "W B",
     "C", "C", "W C", "W C", "W+ C", "B C", "B C", "B+ C", "W B C", "W+ B+ C",
     "W S", "B S", "W+ B S", "W P", "B P", "W B+ P",
     "W B M",
+    "W A", "B A", "W B A",
     "T",
     "W B P", "W W+ P H", "B P A",
     "W B S", "B B+ S C", "W S A",
